ee717a0b6d41d134bdfe68c27fa4f445954f47a0
narwhal04 <q.quek7@gmail.com>
2022-04-01 13:09:56 +0800

    small comments

--- Activity1/channels.go
+++ Activity1/channels.go
@@ -5,14 +5,15 @@ import "fmt"
 func producer(channel chan int) {
 	for i := 0; i < 10; i++ {
 		channel <- i
+		fmt.Println(i)
 	}
 
 	close(channel)
 }
 
 func main() {
-	ch := make(chan int)
-	go producer(ch)
+	ch := make(chan int, 10)
+	go producer(ch) //secondary thread due to function call
 	for {
 		v, ok := <-ch
 		if ok == false {
