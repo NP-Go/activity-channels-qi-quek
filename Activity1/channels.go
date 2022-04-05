f9d5520689fccca178e13178332954b8e7904a15
narwhal04 <q.quek7@gmail.com>
2022-04-05 19:18:13 +0800

    added further self comment

--- Activity1/channels.go
+++ Activity1/channels.go
@@ -14,10 +14,10 @@ func producer(channel chan int) {
 func main() {
 	ch := make(chan int, 10)
 	go producer(ch) //secondary thread due to function call
-	for {
+	for {           //using as a while loop
 		v, ok := <-ch
-		if ok == false {
-			break
+		if ok == false { //adding if statement with condition
+			break //this breaks the while loop
 		}
 		fmt.Println("Received ", v, ok)
 	}
